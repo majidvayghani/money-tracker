47b2d8fa960e414620349521e8a3f29ec5e11348
majid vayghani <mjvayghani@gmail.com>
2024-08-27 15:57:21 +0330

    register a simple admin. this needs to be improved

--- core/transactions/admin.py
+++ core/transactions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction
 
 # Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['date', 'created_at']
+
+admin.site.register(Transaction, TransactionAdmin)
