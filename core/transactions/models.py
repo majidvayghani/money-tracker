24526228e40abc5f39aacf15e0c331dc7ed0392e
majid vayghani <mjvayghani@gmail.com>
2024-08-27 15:51:38 +0330

    Updated  to inherit from (core/utils/basemodel.py) for automatic timestamp management.

--- core/transactions/models.py
+++ core/transactions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+from  core.utils.basemodel import BaseModel
+
+
+class Transaction(BaseModel):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    tag = models.CharField(max_length=50, blank=True, null=True)
+
+
+    def __str__(self):
+        return f'{self.category} - {self.amount}'
