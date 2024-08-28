cbbe556d7895b207a100d5817c5a31226a863fc4
majid vayghani <mjvayghani@gmail.com>
2024-08-28 09:27:27 +0330

    Add basic CRUD API endpoints for Transaction model in transactions/api/v1

---
+++ core/transactions/api/v1/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import transaction_list, transaction_detail
+
+urlpatterns = [
+    path('', transaction_list, name = 'transaction-list'),
+    path('<uuid:pk>/', transaction_detail, name = 'transaction-detail'),
+]
