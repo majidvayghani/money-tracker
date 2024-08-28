72e12daadbea7f90105f62b1e005bc1ccaae9d56
majid vayghani <mjvayghani@gmail.com>
2024-08-28 09:49:54 +0330

    Configure URL for transaction/api/v1/urls in core/urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('transactions/api/v1/', include('transactions.api.v1.urls')) #  Include the transactions API-V1 URLs
 ]
