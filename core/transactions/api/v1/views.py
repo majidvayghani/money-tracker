87186651b426c21771fef869327e38640cbc9d1a
majid vayghani <mjvayghani@gmail.com>
2024-08-29 15:45:55 +0330

    Update  with improved error handling and response

--- core/transactions/api/v1/views.py
+++ core/transactions/api/v1/views.py
@@ -1,13 +1,13 @@
-from rest_framework.decorators import api_view 
-from rest_framework.response import Response 
-from rest_framework import status 
-  
-from transactions.models import Transaction 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from transactions.models import Transaction
 from .serializers import TransactionSerializer
 
 @api_view(['GET', 'POST'])
 def transaction_list(request):
-    """" List all transactions or create a new transactions """
+    """List all transactions or create a new transaction"""
 
     if request.method == 'GET':
         transactions = Transaction.objects.all()
@@ -19,17 +19,16 @@ def transaction_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def transaction_detail(request, pk):
-    """ Retrieve, update or delete a transaction """
+    """Retrieve, update or delete a transaction"""
 
     try:
-        transaction = Transaction.objects.get(pk=pk) # retrive a transaction using pk
+        transaction = Transaction.objects.get(pk=pk)
     except Transaction.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"detail": "Transaction not found."}, status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = TransactionSerializer(transaction)
@@ -39,7 +38,7 @@ def transaction_detail(request, pk):
         serializer = TransactionSerializer(transaction, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
