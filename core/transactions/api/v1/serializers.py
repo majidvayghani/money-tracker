dc2d9d62959551174089bf85848529e40a1a7f71
majid vayghani <mjvayghani@gmail.com>
2024-08-29 15:47:52 +0330

    Add validate_amount method and read_only_fields.

--- core/transactions/api/v1/serializers.py
+++ core/transactions/api/v1/serializers.py
@@ -6,3 +6,11 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Transaction 
         fields = "__all__"
+
+        read_only_fields = ('created_at', 'updated_at')
+
+        def validate_amount(self, value):
+            """validation methods"""
+            if value <= 0:
+              raise serializers.ValidationError("Amount must be greater than zero.")
+            return value
