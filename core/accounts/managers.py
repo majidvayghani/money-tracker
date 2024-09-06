3c637a3bc6831034e92dc8a5e7314ca2291d9852
majid vayghani <mjvayghani@gmail.com>
2024-09-06 16:25:14 +0330

    add comments for better understanding

--- core/accounts/managers.py
+++ core/accounts/managers.py
@@ -2,8 +2,15 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+   
     def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, password and extra_fields.
+        """
         if not email:
             raise ValueError("The Email must be set")
         
