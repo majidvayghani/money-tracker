9d52fce15ea0dc93315d2385544e8dc2b07daec7
majid vayghani <mjvayghani@gmail.com>
2024-09-06 15:46:39 +0330

    add CustomUser class and AUTH_USER_MODEL = 'accounts.CustomUser' to settings.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+ 
+from .managers import CustomUserManager
+
+
+class CustomUser (AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
