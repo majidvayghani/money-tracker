2a8dcfceb7de9b72db42948bdd630c97a13fbd1e
majid vayghani <mjvayghani@gmail.com>
2024-09-09 03:56:32 +0330

    add url for Login API

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('users/', views.create_user, name='create_user'),
-    #path('user/<int:pk>/', views.get_user, name='get_user'),
+    path('login/', views.login_user, name='login'),
 ]
