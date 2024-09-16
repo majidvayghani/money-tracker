37da3d9b0d176b20572237664ef9266ea7f3efdb
majid vayghani <mjvayghani@gmail.com>
2024-09-16 04:46:59 +0330

    add path for TokenAuthentication.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 from . import views
 
 urlpatterns = [
-    path('auth/register/', views.create_user, name='create_user'),
-    path('auth/login/', views.login_user, name='login_user'),
+    path('auth/register/', views.create_user, name='create-user'),
+    # path('auth/login/', views.login_user_v1, name='login-user'),
+    path('auth/login', obtain_auth_token, name='token-login')
 ]
