9919408c583228877d0cb1b876d4c281f156dfcd
Majid Vayghani <mjvayghani@gmail.com>
2024-09-30 13:01:02 +0330

    update urls

--- core/accounts/api/v2/urls.py
+++ core/accounts/api/v2/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SigninAuthToken, UserProfileAPIView, SignoutAPIView
+from .views import SigninAuthToken, SignupAPIview, UserGetOrUpdateOrDeleteAPIView, SignoutAPIView
 
 urlpatterns = [
-    path('signin/', SigninAuthToken.as_view(), name='signin'),
-    path('profile/', UserProfileAPIView.as_view(), name='profile'),
-    path('signout/', SignoutAPIView.as_view(), name='signout'),
+    path('signin', SigninAuthToken.as_view(), name='signin'),
+    path('signup', SignupAPIview.as_view(), name='signup'),
+    path('signout', SignoutAPIView.as_view(), name='signout'),
+    path('profile', UserGetOrUpdateOrDeleteAPIView.as_view(), name='profile'),
 ]
