facdd7512e5d44005466969c47d74073f8eabae6
Majid Vayghani <mjvayghani@gmail.com>
2024-09-30 11:26:57 +0330

    Handle timestamps and soft deletion in general

--- core/core/utils/basemodel.py
+++ core/core/utils/basemodel.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class TimeStampable(models.Model):
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        
+        class Meta:
+            abstract = True
 
+class SoftDeletes(models.Model):
+    deleted_at = models.DateTimeField(null=True)
+    
     class Meta:
-        """
-            the abstract = True option in the Meta class of a model is 
-            used to define an abstract base class. An abstract base class
-            is a model that is not meant to be instantiated or directly used to create database tables.
-            
-        """
         abstract = True
