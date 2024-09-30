6ae086ed9de29f110f5df178648dfaad4e172d6c
Majid Vayghani <mjvayghani@gmail.com>
2024-09-30 12:54:58 +0330

    Update the user model and create a method to retrieve the user object.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -2,12 +2,15 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 import uuid
 
+from core.utils.basemodel import Model
 from .managers import UserManager
 
 
-class User (AbstractBaseUser, PermissionsMixin):
+class User (AbstractBaseUser, PermissionsMixin, Model):
     _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=125, null=False)
+    last_name = models.CharField(max_length=125, null=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     
@@ -18,3 +21,11 @@ class User (AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+    @classmethod
+    def get_user_by_email(cls, email):
+        try:
+            user = cls.objects.get(email=email)
+            return user
+        except cls.DoesNotExist:
+            return None
