bd759b73b19d66d2c4b0648f7e8abd2d4fc93d45
Majid Vayghani <mjvayghani@gmail.com>
2024-10-01 17:58:38 +0330

    update Transaction Model. add the get_transaction_by_id method and _user FK.

--- core/transactions/models.py
+++ core/transactions/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 import uuid
 
-from  core.utils.basemodel import BaseModel
+from core.utils.basemodel import Model
+from accounts.models import User
 
-
-class Transaction(BaseModel):
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Transaction(Model):
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    _user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50)
     description = models.TextField()
     tag = models.CharField(max_length=50, blank=True, null=True)
 
-
     def __str__(self):
         return f'{self.category} - {self.amount}'
+    
+    @classmethod
+    def get_transaction_by_id(cls, id):
+        try:
+            transaction = cls.objects.get(id=id)
+            return transaction
+        except cls.DoesNotExist:
+            return None
