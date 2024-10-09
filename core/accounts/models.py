b66c81e23d7bf89fe998024926523863b2a197cd
Majid Vayghani <mjvayghani@gmail.com>
2024-10-09 23:51:25 +0330

    Add Profile model with OneToOne relationship to User Model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 import uuid
 
 from core.utils.basemodel import Model
@@ -9,8 +12,6 @@ from .managers import UserManager
 class User (AbstractBaseUser, PermissionsMixin, Model):
     _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=125, null=False)
-    last_name = models.CharField(max_length=125, null=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     
@@ -37,3 +38,19 @@ class User (AbstractBaseUser, PermissionsMixin, Model):
         if user:
             return user._id
         return None
+
+
+class Profile(Model):
+    """
+        Advantages:
+        - Separation of Concerns: Keeps authentication-related fields separate from user profile information.
+        - Flexibility: Allows adding or modifying profile fields without altering the User model.
+        - Scalability: Facilitates easier management and scaling of user-related data.
+    """
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    _user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=125, null=False)
+    last_name = models.CharField(max_length=125, null=False)
+
+    def __str__(self):
+        return f"{self._user.email}'s Profile"
