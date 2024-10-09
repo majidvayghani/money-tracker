4bbec4f25de3f9b9bf0c0717abbbc2b2974cdc73
Majid Vayghani <mjvayghani@gmail.com>
2024-10-09 16:12:18 +0330

    Add origin field to check if a transaction is retrieved from cache or database

--- core/transactions/api/v3/serializers.py
+++ core/transactions/api/v3/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers 
 from transactions.models import Transaction 
   
-class TransactionSerializer(serializers.ModelSerializer): 
+class TransactionSerializer(serializers.ModelSerializer):
+    origin  = serializers.SerializerMethodField()
+
     class Meta: 
         model = Transaction 
-        fields = ['amount','description', 'category', 'created_at', 'deleted_at', '_user']
+        fields = ['amount','description', 'category', 'created_at', 'deleted_at', '_user', 'origin']
 
-        read_only_fields = ['_user', 'created_at', 'deleted_at']
+        read_only_fields = ['_user', 'created_at', 'deleted_at', 'origin']
+
+    def get_origin(self, obj):
+        # Access context to modify extra_field dynamically
+        return self.context.get('origin', 'db ')
+         
 
     def validate_amount(self, value):
         """validation methods"""
