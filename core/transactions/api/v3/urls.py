5c132b7d0e67f103f69f61e089e9e962ffd4803b
Majid Vayghani <mjvayghani@gmail.com>
2024-10-17 17:25:50 +0330

    refactor(api):Remove api/v2 to follow the structure of using api/v1 as function-based and api/v2 as class-based. Rename api/v3 to api/v2

--- core/transactions/api/v3/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import TransactionDetail, TransactionCreateView
-
-urlpatterns = [
-    path('', TransactionDetail.as_view(), name = 'transaction-list'),
-    path('create', TransactionCreateView.as_view(), name = 'transaction-list'),
-    path('<uuid:pk>', TransactionDetail.as_view(),name = 'transaction-detail'),
-]
