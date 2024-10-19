a5d05f6df87e3b597130cc724f8a214b18ebaf7f
Majid Vayghani <mjvayghani@gmail.com>
2024-10-20 02:21:26 +0330

    feat(rabbitmq): implement connection and messaging functions in rabbitmq.py

---
+++ core/accounts/api/v2/rabbitmq.py
@@ -0,0 +1,29 @@
+import pika
+import json
+
+def get_rabbitmq_connection():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    return connection, channel
+
+def send_message(queue, message):
+    connection, channel = get_rabbitmq_connection()
+
+    channel.queue_declare(queue='queue')
+    message = json.dumps(message)
+    channel.basic_publish(exchange='', routing_key=queue, body=message)
+
+    connection.close()
+
+def consume_message(queue, callback):
+    connection, channel = get_rabbitmq_connection()
+    channel.queue_declare(queue=queue)
+
+    def on_message(ch, method, properties, body):
+        callback(json.loads(body))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(queue=queue, on_message_callback=on_message)
+    print(f" [*] Waiting for messages in queue '{queue}'. To exit press CTRL+C")
+    channel.start_consuming()
