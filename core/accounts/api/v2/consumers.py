4cce1433af92d1dff065088b52fda5121cab36ec
Majid Vayghani <mjvayghani@gmail.com>
2024-10-23 16:39:02 +0330

    refactor(consumer): change consumer structure and set persistent delivery mode

--- core/accounts/api/v2/consumers.py
+++ core/accounts/api/v2/consumers.py
@@ -15,9 +15,10 @@ i don’t know why I have to do the above configuration
 
 """
 
-from rabbitmq import consume_message
+from rabbitmq import get_rabbitmq_connection
 from django.core.mail import send_mail
 from core.settings.base import *
+import json
 
 def process_signup_message(message):
     email = message.get('email')
@@ -30,6 +31,19 @@ def send_email(to_email):
     from_email = DEFAULT_FROM_EMAIL
  
     send_mail(subject, message, from_email, [to_email])
+
+def consume_message(queue, callback):
+    connection, channel = get_rabbitmq_connection()
+    channel.queue_declare(queue=queue, durable=True)
+
+    def on_message(ch, method, properties, body):
+        callback(json.loads(body))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue=queue, on_message_callback=on_message)
+    print(f" [*] Waiting for messages in queue '{queue}'. To exit press CTRL+C")
+    channel.start_consuming()
 
 if __name__ == '__main__':
     consume_message('signup_queue', process_signup_message)
--- core/accounts/api/v2/rabbitmq.py
+++ core/accounts/api/v2/rabbitmq.py
@@ -12,20 +12,8 @@ def send_email_message(queue, message):
 
     channel.queue_declare(queue='queue', durable=True)
     message = json.dumps(message)
-    channel.basic_publish(exchange='', routing_key=queue, body=message,
-                          delivery_mode = pika.DeliveryMode.Persistent)
-
+    # Set delivery properties with persistent delivery mode
+    properties = pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent)
+    
+    channel.basic_publish(exchange='', routing_key=queue, body=message, properties=properties)
     connection.close()
-
-def consume_message(queue, callback):
-    connection, channel = get_rabbitmq_connection()
-    channel.queue_declare(queue=queue, durable=True)
-
-    def on_message(ch, method, properties, body):
-        callback(json.loads(body))
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=queue, on_message_callback=on_message)
-    print(f" [*] Waiting for messages in queue '{queue}'. To exit press CTRL+C")
-    channel.start_consuming()
