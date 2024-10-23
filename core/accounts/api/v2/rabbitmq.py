dd5e739210ba61bd74002266de862607ff96f9c0
Majid Vayghani <mjvayghani@gmail.com>
2024-10-23 12:19:36 +0330

    feat(rabbitmq): ensure queue and message durability on RabbitMQ restart
    - Declared queue as durable to ensure it survives RabbitMQ node restarts.
    - Marked messages as persistent by setting the delivery_mode to pika.DeliveryMode.Persistent; ensuring messages aren't lost even if RabbitMQ quits or crashes.

--- core/accounts/api/v2/rabbitmq.py
+++ core/accounts/api/v2/rabbitmq.py
@@ -10,15 +10,16 @@ def get_rabbitmq_connection():
 def send_message(queue, message):
     connection, channel = get_rabbitmq_connection()
 
-    channel.queue_declare(queue='queue')
+    channel.queue_declare(queue='queue', durable=True)
     message = json.dumps(message)
-    channel.basic_publish(exchange='', routing_key=queue, body=message)
+    channel.basic_publish(exchange='', routing_key=queue, body=message,
+                          delivery_mode = pika.DeliveryMode.Persistent)
 
     connection.close()
 
 def consume_message(queue, callback):
     connection, channel = get_rabbitmq_connection()
-    channel.queue_declare(queue=queue)
+    channel.queue_declare(queue=queue, durable=True)
 
     def on_message(ch, method, properties, body):
         callback(json.loads(body))
