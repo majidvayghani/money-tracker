fb5a2aa306e7d008f95c8e708a199ba2d785d467
majid vayghani <mjvayghani@gmail.com>
2025-04-25 02:17:52 +0330

    feat(tokens): register Token model in admin panel

--- core/tokens/admin.py
+++ core/tokens/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# tokens/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Token
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('_id', '_user', 'token', 'is_active', 'expired_at', 'created_at')
+    list_filter = ('is_active', 'expired_at', 'created_at')
+    search_fields = ('_user__email', 'token')
+    readonly_fields = ('_id', 'token', 'created_at', 'updated_at')
