78eceb8143d885e3493ae805cfbdbf52ab686c39
majid vayghani <mjvayghani@gmail.com>
2025-04-25 02:16:06 +0330

    refactor(tokens): rename AccessToken to Token and update token generation logic

--- core/tokens/models.py
+++ core/tokens/models.py
@@ -1,14 +1,30 @@
 import uuid
+import secrets
 from django.db import models
 from django.conf import settings
 from core.utils.basemodel import Model
+from django.utils import timezone
+from datetime import timedelta
 
-class AccessToken(Model):
+class Token(Model):
     _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    _user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='custom_tokens')
-    token = models.CharField(max_length=64, unique=True)
-    expired_at = models.DateTimeField(null=True, blank=True)
-    is_active = models.BooleanField(default=True)
+    _user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tokens'
+    )
+    token = models.CharField(max_length=255, unique=True, editable=False)
+    expired_at = models.DateTimeField()
 
     def __str__(self):
         return f'{self._user.email} - {self.token}'
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = secrets.token_urlsafe(32)
+        if not self.expired_at:
+            self.expired_at = timezone.now() + timedelta(hours=1)
+        super().save(*args, **kwargs)
+
+    def is_expired(self):
+        return timezone.now() > self.expired_at
