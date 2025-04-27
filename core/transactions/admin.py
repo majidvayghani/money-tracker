fd74a444bcbfd468d7c8512fb5676e2477c46d59
majid vayghani <mjvayghani@gmail.com>
2025-04-27 23:32:05 +0330

    feat: add admin panel configuration for category

--- core/transactions/admin.py
+++ core/transactions/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Transaction
+from .models import Transaction, TransactionCategory
 
 # Register your models here.
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['amount', '_category', 'description', '_id', '_user']
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'is_income', 'is_expense', '_id']
+
 admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(TransactionCategory, CategoryAdmin)
